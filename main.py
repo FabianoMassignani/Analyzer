c6055168601da559375b60148851b70d7ec23cfb
FabianoMassignani <fabiano-smi@hotmail.com>
2024-06-20 21:16:27 -0300

    refs #1

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import asyncio
 from pyppeteer import launch
-from bs4 import BeautifulSoup
 from telegram import Bot
+import re
+from bs4 import BeautifulSoup
 
 linkedin_url = "https://www.linkedin.com/jobs/search/?alertAction=viewjobs&currentJobId=3954919377&f_TPR=r86400&f_WT=2&geoId=106057199&keywords=Desenvolvedor%20full%20stack&origin=JOB_SEARCH_PAGE_SEARCH_BUTTON&refresh=true&sortBy=DD"
 telegram_token = ""
@@ -22,29 +23,32 @@ async def fetch():
 def extract_jobs(content):
     print("Extraindo vagas...")
     try:
-        soup = BeautifulSoup(content, "html.parser")
-        jobs = soup.find_all("li", class_="job-card-list__job-card-container")
-
         job_details = []
 
-        for job in jobs:
-            job_title = job.find("a", class_="job-card-list__title").text.strip()
-            company = job.find(
-                "span", class_="job-card-container__primary-description"
-            ).text.strip()
-            location = job.find(
-                "li", class_="job-card-container__metadata-item"
-            ).text.strip()
-            job_link = (
-                "https://www.linkedin.com"
-                + job.find("a", class_="job-card-list__title")["href"]
-            )
+        ul_regex = r'<ul\s+class="jobs-search__results-list"[^>]*>(.*?)</ul>'
+        ul_match = re.search(ul_regex, content, re.DOTALL)
 
-            job_details.append(
-                f"Vaga: {job_title}\nEmpresa: {company}\nLocalização: {location}\nLink: {job_link}\n"
-            )
+        if ul_match:
+            ul_content = ul_match.group(1)
 
-        message = "\n".join(job_details)
+            soup = BeautifulSoup(ul_content, "html.parser")
+
+            for li in soup.find_all("li"):
+                job_title = li.find("h3").get_text().strip()
+                company_name = li.find("h4").get_text().strip()
+                location = (
+                    li.find("span", class_="job-search-card__location")
+                    .get_text()
+                    .strip()
+                )
+                job_link = li.find("a")["href"]
+
+                print(
+                    f"Vaga: {job_title}\nEmpresa: {company_name}\nLocalização: {location}\nLink: {job_link}\n"
+                )
+
+        else:
+            print("Não foi possível encontrar a lista de vagas.")
 
     except Exception as e:
         print("Erro durante extração de vagas:", e)
@@ -55,21 +59,14 @@ def send_telegram_notification(message):
     bot.send_message(chat_id=chat_id, text=message)
 
 
-def job():
+async def job():
     print("Rodando script de coleta de dados...")
-    content = asyncio.run(fetch())
+    content = await fetch()
     extract_jobs(content)
 
 
 def main():
-    # Executar o job imediatamente ao iniciar o script
-    job()
-
-    # Loop para manter o script em execução (descomentado se desejado)
-    # schedule.every(1).hour.do(job)
-    # while True:
-    #     schedule.run_pending()
-    #     time.sleep(1)
+    asyncio.get_event_loop().run_until_complete(job())
 
 
 if __name__ == "__main__":
